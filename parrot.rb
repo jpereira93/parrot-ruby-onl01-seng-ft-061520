299957052a74e3011fd152321f20ba44acabf8e9
Jessica Pereira <jcpereira070@gmail.com>
2020-06-16 20:08:03 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot
+  puts "Squawk!"
+end
+
+parrot
+
+def parrot(name)
+  puts "#{name}"
+end
+
+parrot("Squawk!")
